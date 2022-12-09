5629731446b3f43ae7a25100b0f1c343f7dbeb70
Tibo Ulens <ulens.tibo@gmail.com>
2022-12-09 03:50:02 +0100

    update test code to use received dimensions

--- fstest.py
+++ fstest.py
@@ -5,6 +5,7 @@ import random
 random.seed(22)
 
 sockets = []
+dims = []
 colors = []
 start_port = 8000
 
@@ -12,24 +13,25 @@ partitions = 4
 
 for i in range(partitions):
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"connecting to socket at {start_port + i}")
     clientSocket.connect(("127.0.0.1", start_port + i))
+    w, h = struct.unpack('>HH', clientSocket.recv(4))
+    dims.append((w, h))
+    print(w, h)
     sockets.append(clientSocket)
     colors.append(None)
     # colors.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
-w = 840
-h = 525
-
 while True:
     for i in range(partitions):
         colors[i] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        print(f"Drawing section {i}")
+        # print(f"Drawing section {i}")
 
         c_socket = sockets[i]
         packet = []
         coords = []
-        for x in range(w):
-            for y in range(h):
+        for x in range(dims[i][0]):
+            for y in range(dims[i][1]):
                 coords.append((x, y))
 
         # random.shuffle(coords)
